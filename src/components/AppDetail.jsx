cbb26095d57df4553cc51f7624d0c3ecd0fb7b7e
watanabe12 <o8o6o191108@gmail.com>
2023-12-28 12:35:58 +0900

    遷移画面の動画再生処理を追加

--- src/components/AppDetail.jsx
+++ src/components/AppDetail.jsx
@@ -1,66 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Styles from "../styles/AppDetail.module.css";
-import { TopMovieImage } from "./TopMovieImage";
 import { MovieDetail } from "./MovieDetail";
 
 const AppDetail = () => {
   // stateの初期化
-  const [trailerUrl, setTrailerUrl] = useState("");
   const [movie, setMovie] = useState("");
   const [genres, setGenres] = useState([]);
 
   // レンダリング完了時にURLを取得
   useEffect(() => {
-    // URLを取得
-    let url = new URL(window.location.href);
-    // URLSearchParamsオブジェクトのgetメソッドにてパラメータを指定取得
-    setMovie(JSON.parse(url.searchParams.get('movie')));
-    setGenres(JSON.parse(url.searchParams.get('genreName')));
-  },[]);
-
-  console.log('movie')
-  console.log(movie)
-  console.log('genres')
-  console.log(genres)
-
-  // tod再生ボタンクリック後
-  const playButtonClick = (movie) => {
-    // 再度動画クリック時、再表示されるようにtrailerUrlの初期化
-    if (trailerUrl) {
-      setTrailerUrl("");
-    } else {
-      try {
-        //動画を取得
-        let trailerurl = axios.get(`/movie/${movie.id}/videos?api_key=`+ API_KEY);
-        //動画の配列の0番目のkeyにYouTubeのidが入っているので代入
-        setTrailerUrl(trailerurl.data.results[0].key);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        console.log("例外実行");
-      }
+    console.log('window')
+    console.log(window)
+    if (typeof window !== "undefined") {
+      // URLを取得
+      let url = new URL(window.location.href);
+      // URLSearchParamsオブジェクトのgetメソッドにてパラメータを指定取得
+      setMovie(JSON.parse(url.searchParams.get('movie')));
+      setGenres(JSON.parse(url.searchParams.get('genreName')));
     }
-    // movieオブジェクトから予告URLの検索
-    movieTrailer(movie.name || movie.title || movie.original_name || "")
-      .then((url) => {
-        // urlが存在する場合に検索を実施
-        if(url){
-          // URLのパラメータvキー部分の切り出し取得
-          let urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get("v"));
-        }
-      })
-
-    console.log("trailerUrl")
-    console.log(trailerUrl)
-  };
+  },[]);
 
   return (
     <div className={Styles.AppDetail}>
-      <TopMovieImage movie={movie}/>
       <MovieDetail
         movie={movie}
-        genres={genres}/>
+        genres={genres}
+        />
     </div>
   );
 }
--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -17,34 +17,3 @@ const PlayButton = ({ onClick }) => {
 }
 
 export default PlayButton;
-  // return (
-  //   <Button
-  //     color="inherit"
-  //     variant="contained"
-  //     startIcon={
-  //       <PlayArrowIcon
-  //         sx={{
-  //           fontSize: {
-  //             xs: "24px !important",
-  //             sm: "32px !important",
-  //             md: "40px !important",
-  //           },
-  //         }}
-  //       />
-  //     }
-  //     {...others}
-  //     sx={{
-  //       px: { xs: 1, sm: 2 },
-  //       py: { xs: 0.5, sm: 1 },
-  //       fontSize: { xs: 18, sm: 24, md: 28 },
-  //       lineHeight: 1.5,
-  //       fontWeight: "bold",
-  //       whiteSpace: "nowrap",
-  //       textTransform: "capitalize",
-  //       ...sx,
-  //     }}
-  //     onClick={() => navigate(`/${MAIN_PATH.watch}`)}
-  //   >
-  //     Play
-  //   </Button>
-  // );
